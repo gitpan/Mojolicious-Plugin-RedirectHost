eacd6dd3b7305a69f78afbbadd41fa30478dac78
Alex <alexbyk@cpan.org>
2014-04-22 02:49:21 +0000

    Import of ALEXBYK/Mojolicious-Plugin-RedirectHost-1.00 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-RedirectHost
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         ALEXBYK/Mojolicious-Plugin-RedirectHost-1.00.tar.gz
    gitpan-cpan-author:       ALEXBYK
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/RedirectHost.pm
+++ lib/Mojolicious/Plugin/RedirectHost.pm
@@ -3,11 +3,12 @@ package Mojolicious::Plugin::RedirectHost;
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::URL;
 
-our $VERSION = '0.05';
+our $VERSION = '1.00';    # VERSION
 
 # where to look for options
 my $CONFIG_KEY   = 'redirect_host';
 my $DEFAULT_CODE = 301;
+my $EXCEPT_PATH  = '/robots.txt';
 
 sub register {
   my ($self, $app, $params) = @_;
@@ -21,7 +22,9 @@ sub register {
   }
 
   unless ($options{host}) {
-    $app->log->error('RedirectHost plugin: define "host" option at least!');
+    my $msg = 'RedirectHost plugin: define "host" option at least!';
+    $app->log->error($msg) unless $options{silent};
+
     return;
   }
 
@@ -35,46 +38,16 @@ sub register {
       # don't need redirection
       return if $url->host eq $options{host};
 
-      # TODO: check if except_path is RE or ARR
-      # path match exception
-      if (my $except_path = $options{except_path}) {
-        return if $path eq $except_path;
+      # except_robots?
+      if ($options{er}) {
+        return if $path eq $EXCEPT_PATH;
       }
 
       # main host
       $url->host($options{host});
 
-      #$url->host(delete local $options{host});
-
       # code
       $c->res->code($options{code} || $DEFAULT_CODE);
-
-      #$c->res->code(delete local $options{code} || $DEFAULT_CODE);
-
-      if (ref $options{url} eq 'HASH') {
-
-        # query
-        if (ref $options{url}->{query} eq 'ARRAY') {
-          my @query = @{delete $options{url}->{query}};
-          $url->query(@query);
-
-        }
-
-        # замещаем значения
-        foreach my $what (keys %{$options{url}}) {
-          $url->$what($options{url}->{$what}) if $options{url}->{$what};
-        }
-      }
-      elsif (ref $options{url}) {
-
-        # replace a whole url with a passed Mojo::URL object
-        $url = $options{url};
-      }
-      elsif ($options{url}) {
-
-        # replace a whole url with a new one
-        $url = Mojo::URL->new($options{url});
-      }
 
 
       $c->redirect_to($url->to_string);
@@ -84,6 +57,15 @@ sub register {
   return;
 }
 
+1;
+
+# ABSTRACT: Redirects requests from mirrors to the main host (useful for SEO)
+
+__END__
+
+=pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -91,21 +73,21 @@ Mojolicious::Plugin::RedirectHost - Redirects requests from mirrors to the main 
 
 =head1 VERSION
 
-Version 0.05
-
+version 1.00
 
 =head1 SYNOPSIS
 
+Generates 301 redirect from C<http://mirror.main.host/path?query> to C<http://main.host/path?query>
 
-Generate 301 redirect from C<http://mirror.main.host/path?query> to C<http://main.host/path?query>
-  
   # Mojolicious
   $app->plugin('RedirectHost', host => 'main.host');
   
   # Mojolicious::Lite
   plugin RedirectHost => { host => 'main.host' };
 
-All requests with C<Host> header not equal to the C<host> option will be redirected to the main host
+All requests with C<Host> header that are not equal to the C<host> option will be redirected to the main host
+It would be better if you'll be using per mode config files (your_app.production.conf etc). This would make possible
+to redirect only in production enviropment (but do nothing while coding your app)
 
 =head1 OPTIONS/USAGE
 
@@ -120,69 +102,26 @@ This option is required. Without it plugin do nothing
 
 Type of redirection. Default 301 (Moved Permanently)
 
-=head2 C<except_path>
+=head2 C<er> (except /rotots.txt)
 
-  $app->plugin('RedirectHost', host => 'main.host', except_path => '/robots.txt');
+  $app->plugin('RedirectHost', host => 'main.host', er => 1);
 
-If the path of the request will match C<except_path> value, redirection will be avoid.
-Path must begin with leading '/';
-Usefull to avoid /robots.txt redirections
-In the future maybe I'll make this parameter more flexible
+If true, requests like /robots.txt will not be redirected but rendered. That's for Yandex search engine.
+If you want to change a domain but worry about yandex TIC, it's recomended to make it possible for Yandex to read your robots.txt
+with new Host directive. If so, that's exactly what you're looking for
 
-=head2 C<url>
+=head2 C<silent>
 
-All keys of the C<url> hash (except C<query>) become L<Mojo::URL> object's methods, regarding old request
-  
-  # 302: http://mirror.main.host/path?query -> http://main.host/path?query
-  $app->plugin('RedirectHost', host => 'main.host', code => 302);
+If C<silent> is true, doesn't write messages to the error log even if L</host> is missing.
+Default value is C<false>
 
-You can replace some parts of the old request, for example scheme (C<https>), or add extra query parameters C<?a=b> to the end
+You can configure plugin in a production config file and define C<silent> in a development config.
 
-  # http://mirror.main.host/foo -> https://main.host/foo?a=b
-  $app->plugin(
-    'RedirectHost',
-    host   => 'main.host',    
-    url => { scheme => 'https', query  => [{a => 'b'}] }
-  );
+	# app.production.conf
+	# redirect_host => {host => 'main.host'},
 
-
-How to use url->{query} option (pay attention to '[]')
-
-  # append ?a=old&foo=bar -> ?a=old&foo=bar&a=b
-  url => {query => [{a => 'b'}]
-
-  # merge ?a=old&foo=bar -> ?a=b&foo=bar
-  url => {query => [[a => 'b']]
-  
-  # replace ?a=old&foo=bar -> ?a=b
-  url => {query => [a => 'b']}
-  
-  # this works too
-  url => {query => [Mojo::Parameters->new(a => 'b')]}
-  
-  # Wrong!!! Don't do this. Don't forget []
-  url => {query => Mojo::Parameters->new(a => 'b')}
-
-See L<Mojo::URL/query>
-
-You can pass a string to the C<url> part of options
-
-  # http://mirror.main.host/foo -> http://google.com
-  $app->plugin(
-    'RedirectHost',
-    host   => 'main.host',    
-    url => 'http://google.com'
-  );
-  
-
-New url as an L<Mojo::URL> object
-
-  # http://mirror.main.host/foo -> http://google.com
-  $app->plugin(
-    'RedirectHost',
-    host => 'main.host',    
-    url  => Mojo::URL->new('http://google.com')
-  );
+	# app.development.conf: 
+	# redirect_host => {silent => 1},
 
 =head1 CONFIG
 
@@ -190,76 +129,24 @@ You can pass options to the plugin with the help of your config. Use C<redirect_
 
   $app->config(redirect_host => {host => 'main.host'});
 
+TIP: use per mode config files (yourapp.production.conf) to pass parameters to the plugin to avoid redirection during
+development process
+
 =head1 METHODS
 
 =head2 register
 
-Register. См L<Mojolicious::Plugin/register>
-
-=head1 TODO
-
-Play around requests without "Host" header like this:
-  
-  GET / HTTP/1.1
-
+Register.  L<Mojolicious::Plugin/register>
 
 =head1 AUTHOR
 
-Alex, C<< <alexbyk at cpan.org> >>
+alexbyk <alex@alexbyk.com>
 
-=head1 BUGS
+=head1 COPYRIGHT AND LICENSE
 
-Please report any bugs or feature requests to C<bug-mojolicious-plugin-redirecthost at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mojolicious-Plugin-RedirectHost>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+This software is copyright (c) 2014 by alexbyk.
 
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Mojolicious::Plugin::RedirectHost
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Mojolicious-Plugin-RedirectHost>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Mojolicious-Plugin-RedirectHost>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Mojolicious-Plugin-RedirectHost>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Mojolicious-Plugin-RedirectHost/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2014 Alex.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
-
-1;    # End of Mojolicious::Plugin::RedirectHost
